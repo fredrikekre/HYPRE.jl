d7bfa2b9e9e11e8c84c2f300194d324ccaaa8f90
Fredrik Ekre <ekrefredrik@gmail.com>
2022-06-14 16:29:26 +0200

    Initial Clang.jl generator.

---
+++ gen/generator.jl
@@ -0,0 +1,33 @@
+using Clang.Generators
+using HYPRE_jll, MPICH_jll
+
+cd(@__DIR__)
+
+hypre_include_dir = normpath(HYPRE_jll.artifact_dir, "include")
+mpi_include_dir = normpath(MPICH_jll.artifact_dir, "include")
+
+options = load_options(joinpath(@__DIR__, "generator.toml"))
+
+args = get_default_args()
+push!(args, "-I$(hypre_include_dir)")
+push!(args, "-I$(mpi_include_dir)")
+
+# Compiler flags from Yggdrasil (??)
+# https://github.com/JuliaPackaging/Yggdrasil/blob/9d131ba0e4aa393b00f4d71ef5a3f909419a70a7/H/HYPRE/build_tarballs.jl
+push!(args, "-DHYPRE_ENABLE_SHARED=ON")
+push!(args, "-DHYPRE_ENABLE_HYPRE_BLAS=ON")
+push!(args, "-DHYPRE_ENABLE_HYPRE_LAPACK=ON")
+push!(args, "-DHYPRE_ENABLE_CUDA_STREAMS=OFF")
+push!(args, "-DHYPRE_ENABLE_CUSPARSE=OFF")
+push!(args, "-DHYPRE_ENABLE_CURAND=OFF")
+
+headers = joinpath.(hypre_include_dir, [
+    # "HYPRE.h",
+    # "HYPRE_utilities.h",
+    # "IJ_matrix.h",
+    "HYPRE_IJ_mv.h",
+])
+
+ctx = create_context(headers, args, options)
+
+build!(ctx)
